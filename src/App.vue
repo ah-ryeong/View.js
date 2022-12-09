601509eb650e112d47b4d8c9e586757027a53eb6
Aryeong <aryeong22@hanmail.net>
2022-12-09 22:11:10 +0900

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- html 속성도 데이터바인딩 가능 -> :속성="데이터테이블" -->
+    <h4 :style="styleH">{{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price1 : 60,
+      price2 : 70,
+      styleH : 'color : blue',
+      products : ['역삼동 오피스텔', '천호동 오피스텔', '마포구 오피스텔'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
