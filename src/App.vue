0dd49e3125a5110277366866a9358c4aab378199
Aryeong <aryeong22@hanmail.net>
2022-12-10 22:33:43 +0900

    반복문

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
+  <div class="menu">
+    <a>Home</a>
+    <a>Shop</a>
+    <a>About</a>
+  </div>
+
+  <div v-for="(a, i) in products" :key="i">
     <!-- html 속성도 데이터바인딩 가능 -> :속성="데이터테이블" -->
-    <h4 :style="styleH">{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p>
-  </div>
-
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
-
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>{{ price2 }} 만원</p>
+    <h4 :style="styleH">{{ products[i] }}</h4>
+    <p>{{ price[i] }} 만원</p>
   </div>
 </template>
 
@@ -23,8 +18,7 @@ export default {
   name: 'App',
   data() {
     return {
-      price1 : 60,
-      price2 : 70,
+      price : [60, 70, 100],
       styleH : 'color : blue',
       products : ['역삼동 오피스텔', '천호동 오피스텔', '마포구 오피스텔'],
     }
@@ -41,6 +35,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  text-decoration: none;
+  padding: 10px;
 }
 </style>
