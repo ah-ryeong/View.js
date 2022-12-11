6ae683b26c42666dd39caa99e0bebae46c0cc06a
Aryeong <aryeong22@hanmail.net>
2022-12-11 22:19:35 +0900

    Event Handler

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
     <!-- html 속성도 데이터바인딩 가능 -> :속성="데이터테이블" -->
     <h4 :style="styleH">{{ products[i] }}</h4>
     <p>{{ price[i] }} 만원</p>
+    <button @click="increase(i)">허위매물 신고</button> <span>신고수 : {{ count[i] }}</span>
   </div>
 </template>
 
@@ -21,8 +22,16 @@ export default {
       price : [60, 70, 100],
       styleH : 'color : blue',
       products : ['역삼동 오피스텔', '천호동 오피스텔', '마포구 오피스텔'],
+      count : [0, 0, 0],
     }
   },
+
+  methods : {
+    increase(props) {
+      this.count[props] ++;
+    },
+  },
+
   components: {
   }
 }
