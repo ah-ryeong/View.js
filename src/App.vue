086088342dd5c6f6ab6d38a028fc786924d3f2fc
Aryeong <aryeong22@hanmail.net>
2022-12-13 22:23:37 +0900

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="black-bg" v-if="modal">
+    <div class="white-bg">
+      <h4>Detail Page</h4>
+      <p>detail</p>
+      <button @click="modal = false">close</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a>Home</a>
     <a>Shop</a>
@@ -6,8 +14,9 @@
   </div>
 
   <div v-for="(a, i) in products" :key="i">
+    <img :src="require(`./assets/room${i}.jpg`)" :style="room-img"/>
     <!-- html 속성도 데이터바인딩 가능 -> :속성="데이터테이블" -->
-    <h4 :style="styleH">{{ products[i] }}</h4>
+    <h4 :style="styleH" @click="modal = true">{{ products[i] }}</h4>
     <p>{{ price[i] }} 만원</p>
     <button @click="increase(i)">허위매물 신고</button> <span>신고수 : {{ count[i] }}</span>
   </div>
@@ -23,6 +32,7 @@ export default {
       styleH : 'color : blue',
       products : ['역삼동 오피스텔', '천호동 오피스텔', '마포구 오피스텔'],
       count : [0, 0, 0],
+      modal : false,
     }
   },
 
@@ -38,6 +48,14 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,5 +74,23 @@ export default {
   color: white;
   text-decoration: none;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0, 5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; 
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
