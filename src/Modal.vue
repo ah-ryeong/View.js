26a4b9cbc029433b7ab445dd08630c400672b640
Aryeong <aryeong22@hanmail.net>
2022-12-26 22:43:19 +0900

    input

--- src/Modal.vue
+++ src/Modal.vue
@@ -3,7 +3,9 @@
         <div class="white-bg">
         <h4>{{ products[clickNum].title }}</h4>
         <h4>{{ products[clickNum].content }}</h4>
-        <p>{{ products[clickNum].price }}</p>
+        <!-- <input @input="month = $event.target.value"> -->
+        <input v-model.number="month">
+        <p>{{ month }}개월 선택함 : {{ (products[clickNum].price) * month }}원</p>
         <button @click="$emit('closeModal')">close</button>
 
         <div v-if="clickNum === 1">
@@ -23,6 +25,11 @@
     export default {
         // eslint-disable-next-line vue/multi-word-component-names
         name: 'Modal',
+        data() {
+            return {
+                month : 1,
+            }
+        },
         props : {
             products : Array,
             modal : Boolean,
